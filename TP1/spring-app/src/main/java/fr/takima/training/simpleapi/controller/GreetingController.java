6e1798dc6cfebbf8f71fb2e8aca64adf47c2c0f8
AlikBook <ivanarturograndi6@gmail.com>
2025-10-28 09:22:41 +0100

    Added pull property on the ansible part backend

--- TP1/spring-app/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
+++ TP1/spring-app/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin
 public class GreetingController {
 
     private static final String TEMPLATE = "Bonjour, %s!";
