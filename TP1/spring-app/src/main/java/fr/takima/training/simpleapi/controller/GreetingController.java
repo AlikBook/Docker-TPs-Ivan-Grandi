6e41b110743d69b1221da0b4a5d2b1d2f93fdf37
AlikBook <ivanarturograndi6@gmail.com>
2025-10-28 15:41:44 +0100

    Added config

--- TP1/spring-app/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
+++ TP1/spring-app/src/main/java/fr/takima/training/simpleapi/controller/GreetingController.java
@@ -13,7 +13,7 @@ public class GreetingController {
 
     private static final String INSTANCE = System.getenv().getOrDefault("BACKEND_INSTANCE", "unknown");
 
-    @GetMapping("/greeting")
+    @GetMapping("/")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "Monde") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name) + " from " + INSTANCE);
     }
